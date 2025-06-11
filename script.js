0d08b9c76dbf6446627c3609accf36ecd59f4485
Rodrigo1010-01 <rodrigo.greczysczyn@escola.pr.gov.br>
2025-06-11 20:01:36 -0300

    .

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const flashcards = document.querySelectorAll('.flashcard');
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
 
-    flashcards.forEach(card => {
-        card.addEventListener('click', () => {
-            card.classList.toggle('is-flipped');
-        });
-    });
-});
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
